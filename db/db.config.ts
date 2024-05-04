9748fae3279577f9009a269fddf626c369a5f93f
DiegovillaxD05 <diegovillalbacent1234@gmail.com>
2024-05-04 18:13:23 -0400

    Configuracion para servidor

--- db/db.config.ts
+++ db/db.config.ts
@@ -4,8 +4,11 @@ import { Sequelize } from "sequelize";
 //     host: 'localhost',
 //     dialect:'mysql',
 // })
-const db = new Sequelize('ecommerce_db', 'root', '050698',{
-    host: 'localhost',
+const db = new Sequelize(
+    process.env.DB_NAME || 'ecommerce_db',
+    process.env.DB_USER || 'root', 
+    process.env.DB_PASSWORD || '050698',{
+    host: process.env.DB_HOST || 'localhost',
     dialect:'mysql',
 })
 
