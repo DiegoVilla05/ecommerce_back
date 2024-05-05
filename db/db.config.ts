bbb1e249995cbbf3b2ad68a12c25390efcbf0bce
Christian Parzajuk <111670170+KParza@users.noreply.github.com>
2024-05-05 00:17:56 -0400

    configuracion para railway

--- db/db.config.ts
+++ db/db.config.ts
@@ -1,15 +1,10 @@
 import { Sequelize } from "sequelize";
 
-// const db = new Sequelize('ecommerce_db', 'root', 'iHyI6ROVG9mOCErg',{
+// const db = new Sequelize('ecommerce_db', 'root', '050698',{
 //     host: 'localhost',
 //     dialect:'mysql',
 // })
-const db = new Sequelize(
-    process.env.DB_NAME || 'ecommerce_db',
-    process.env.DB_USER || 'root', 
-    process.env.DB_PASSWORD || '050698',{
-    host: process.env.DB_HOST || 'localhost',
-    dialect:'mysql',
-})
+const db = new Sequelize(process.env.MYSQL_URL || 'mysql://root:iHyI6ROVG9mOCErg@localhost:3306/ecommerce_db')
+
 
 export default db;
